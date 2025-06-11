e41557efc7e4495a5451ac0baff592b4f65ce006
RussTP <rpawlett@live.com>
2025-06-11 13:55:22 -0400

    fixed scoring system, and made round mode so you can now enter overtime and whoever gets the most points that round wins. added nav links to navigate the page easier

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
  let blueCounter = 0;
  let greenCounter = 0;
-
+ let round = 0;
+ let gameOver = false;
  
 
 const btnOneBlue = document.getElementById("blueOneBtn");
@@ -12,19 +13,30 @@ const btnThreeGreen = document.getElementById("greenThreeBtn");
 const greenDisplay = document.getElementById("greenScore");
 
 const outcomeDiv = document.getElementById("outcome");
+const roundOverBtn = document.getElementById("roundOver");
 
 
 
-
+// Checking outcome of the game
 
 function checkOutcome() {
-    if ((blueCounter >= 21 || greenCounter >= 21) && !outcomeDiv.textContent) {
+    if ((blueCounter >= 21 || greenCounter >= 21) && !gameOver) {
         let outcome;
-        if (blueCounter === greenCounter) {
-            outcome = "It's a tie!";
-        } else {
+    if (blueCounter === greenCounter) {
+            tie.textContent = "It's a tie, next highest score wins!";
+            tie.style.height = "100px";
+              tie.style.textAlign = "center";
+        tie.style.justifyContent = "center";
+          tie.style.border = "2px solid red";
+    tie.style.alignItems ="center";
+    tie.style.paddingTop = "42px";
+            return;
+    } 
+        
+    else if (Math.abs(blueCounter - greenCounter) >= 1) {
             const winner = blueCounter > greenCounter ? "Blue" : "Green";
-            outcome = `${winner} won the game!`;
+            outcome = `${winner} team won the game!`;
+            outcomeDiv.textContent = outcome;
             outcomeDiv.style.color = "rgb(82, 59, 58)";
             outcomeDiv.style.backgroundColor = "rgb(230, 188, 137)"
             outcomeDiv.style.display = "flex";
@@ -33,12 +45,15 @@ function checkOutcome() {
             outcomeDiv.style.borderRadius = "50px";
             outcomeDiv.style.border = "3px solid rgb(193, 110, 0)";
             outcomeDiv.style.margin = "5px";
+            roundOverBtn.disabled = true;
+            tie.style.display = "none";
 
-        }
+
+        } else {
         outcomeDiv.textContent = `${outcome} - Final Score Blue Team: ${blueCounter}  Green Team: ${greenCounter}`;
         outcomeDiv.style.padding = "10px";
         
-
+        }
         
         const btnDiv = document.getElementById("outcome");
         const resetBtn =  document.createElement("button");
@@ -54,12 +69,15 @@ function checkOutcome() {
              document.querySelector("#outcome").textContent ="";
              document.querySelector("#blueScore").textContent = "Score: 0"
               document.querySelector("#greenScore").textContent = "Score: 0"
-                 btnOneBlue.disabled = false;
+        
+        btnOneBlue.disabled = false;
         btnThreeBlue.disabled = false;
         btnOneGreen.disabled = false;
         btnThreeGreen.disabled = false;
+        roundOverBtn.disabled = false;
+        roundOverBtn.textContent = "Round Over";
         outcomeDiv.style.display = "none";
-
+      
         }
 
         btnOneBlue.disabled = true;
@@ -74,26 +92,42 @@ function checkOutcome() {
 btnOneBlue.addEventListener("click", () => {
     blueCounter +=  + 1;
     blueDisplay.textContent = "Score: " + blueCounter;
-    checkOutcome();
+  
 });
 
 btnThreeBlue.addEventListener("click", () => {
     blueCounter += 3;
     blueDisplay.textContent = "Score: " + blueCounter;
-    checkOutcome();
+  
 });
 
 btnOneGreen.addEventListener("click", () => {
     greenCounter += 1;
     greenDisplay.textContent = "Score: " + greenCounter;
-    checkOutcome();
+   
 });
 
 btnThreeGreen.addEventListener("click", () => {
     greenCounter += 3;
     greenDisplay.textContent = "Score: " + greenCounter;
-    checkOutcome();
+    
 });
+
+roundOverBtn.addEventListener("click", function () {
+round ++;
+roundOverBtn.textContent = "Round: " + round; 
+
+gameRound();
+});
+
+let lastCheckedRound = 0;
+
+function gameRound() {
+    if(round !== lastCheckedRound) {
+        checkOutcome();
+        lastCheckedRound = round;
+    }
+}
 
 
 const isTouchDevice = "ontouchstart" in window || navigator.msMaxTouchPoints > 0;
